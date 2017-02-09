5b13fa3bbe706119d0d76a10350a347d2ace412c
incometrader <igdefinition@gmail.com>
2017-02-09 11:17:48 +0100

    add 10 tests with jasmine

--- min_max/spec/test-spec.js
+++ min_max/spec/test-spec.js
@@ -16,8 +16,24 @@ var findMinMax = require("../src/minMax");
         expect(findMinMax([6, 4])).toEqual([4, 6]);
       });
 
+      it('should return [2, 9] for [2, 4, 6, 8, 9]', function () {
+        expect(findMinMax([2, 4, 6, 8, 9])).toEqual([2, 9]);
+      });
+
+      it('should return [0, 9] for [2, 1, 3, 9, 0, 5, 7]', function () {
+        expect(findMinMax([2, 1, 3, 9, 0, 5, 7])).toEqual([0, 9]);
+      });
+
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
         expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+      });
+
+      it('should return [5, 100] for [5, 10, 20, 90, 30, 70, 80, 100, 88]', function () {
+        expect(findMinMax([88, 10, 20, 90, 30, 70, 80, 100, 5])).toEqual([5, 100]);
+      });
+
+      it('should return [3, 910] for [4, 910, 720, 790, 430, 70, 80, 100, 88, 105, 306, 609, 3]', function () {
+        expect(findMinMax([4, 910, 720, 790, 430, 70, 80, 100, 88, 105, 306, 609, 3])).toEqual([3, 910]);
       });
 
     });
@@ -26,6 +42,14 @@ var findMinMax = require("../src/minMax");
 
       it('should return [4] for [4, 4, 4, 4]', function () {
         expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
+
+      it('should return [10] for [10, 10, 10]', function () {
+        expect(findMinMax([10, 10, 10])).toEqual([10]);
+      });
+
+      it('should return [55] for [55, 55]', function () {
+        expect(findMinMax([55, 55])).toEqual([55]);
       });
 
     });
