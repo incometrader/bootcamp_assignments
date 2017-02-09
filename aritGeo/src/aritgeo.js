1b2cc9dad3dcd79ad28a422e9ecf6624265b856a
incometrader <igdefinition@gmail.com>
2017-02-09 11:54:18 +0100

    added function for arithGeo

---
+++ aritGeo/src/aritgeo.js
@@ -0,0 +1,21 @@
+function aritGeo(arr) { 
+if (arr === []){
+  return 0;
+}
+var minus = arr[1] - arr[0], divide = arr[1] / arr[0], arith = true, geo = true;
+
+for(var i = 0; i < arr.length - 1; i++){
+    if( arr[i + 1] - arr[i] !== minus )
+      arith = false;
+    if(arr[i + 1] / divide !== arr[i])
+      geo = false;
+}
+if(arith === true)
+    return 'Arithmetic';
+else if(geo === true)
+    return 'Geometric';
+else
+    return -1;
+}
+
+module.exports = aritGeo;
